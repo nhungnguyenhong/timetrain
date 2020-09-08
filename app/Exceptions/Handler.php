sondn-dsvn/training

Handle exception.
Signed-off-by: sondn-dsvn <son-duong@dimage.co.jp>

Init a api error exception and handle exception to returning json
response.

app/Exceptions/Handler.php
@@ -2,11 +2,25 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponsive;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Exception\SuspiciousOperationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponsive;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -50,6 +64,100 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        return $this->handle($exception);
+    }
+
+    private function handle(Throwable $exception)
+    {
+        $exception = $this->prepareException($exception);
+
+        return $this->genericResponse($exception);
+    }
+
+    protected function prepareException(Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException || $exception instanceof SuspiciousOperationException) {
+            $exception = new NotFoundHttpException(__('http_errors.404'), $exception);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            $exception = new AccessDeniedHttpException(__('http_errors.403'), $exception);
+        }
+
+        if ($exception instanceof TokenMismatchException) {
+            $exception = new HttpException(419, __('http_errors.419'), $exception);
+        }
+
+        return $exception;
+    }
+
+    private function genericResponse(Throwable $exception)
+    {
+        return $this->responseError('',
+            $this->convertExceptionToArray($exception),
+            $this->getHeaders($exception),
+            $this->getStatusCode($exception));
+    }
+
+    protected function convertExceptionToArray(Throwable $exception) : array
+    {
+        $responses['code'] = $exception instanceof ApiErrorException ? $exception->getApiCode() : $exception->getCode();
+
+        $statusCode = $this->getStatusCode($exception);
+
+        $responses['message'] = $exception->getMessage() ?: sprintf("%d - %s", $statusCode, __("http_errors.$statusCode"));
+
+        if ($exception instanceof ValidationException) {
+            $responses['errors'] = $exception->errors();
+        }
+
+        if ($this->isRunningInDebugMode()) {
+            $responses['debug'] = $this->buildDebugLog($exception);
+        }
+
+        return $responses;
+    }
+
+    private function isRunningInDebugMode() : bool
+    {
+        return config('app.debug');
+    }
+
+    private function buildDebugLog(Throwable $exception)
+    {
+        $debugLogs = [
+            'message' => $exception->getMessage(),
+            'exception' => get_class($exception),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+            'trace' => explode("\n", $exception->getTraceAsString()),
+        ];
+
+        if ($exception->getPrevious()) {
+            $debugLogs['trace'] = [
+                'current' => explode("\n", $exception->getTraceAsString()),
+                'previous' => explode("\n", $exception->getPrevious()->getTraceAsString())
+            ];
+        }
+
+        return $debugLogs;
+    }
+
+    private function getHeaders(Throwable $exception) : array
+    {
+        return $exception instanceof HttpExceptionInterface ? $exception->getHeaders() : [];
+    }
+
+    private function getStatusCode(Throwable $exception) : int
+    {
+        if ($exception instanceof ValidationException) {
+            return $exception->status;
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return Response::HTTP_UNAUTHORIZED;
+        }
+
+        return $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : Response::HTTP_NOT_FOUND;
     }
 }

nhungnguyenhong/timetrain

app/Exceptions/Handler.php
@@ -2,11 +2,25 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use App\Traits\ApiResponsive;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Exception\SuspiciousOperationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Exception;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponsive;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -29,7 +43,7 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return void
      *
      * @throws \Exception
@@ -43,13 +57,107 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return \Symfony\Component\HttpFoundation\Response
      *
-     * @throws \Exception
+     * @throws \Throwable
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        return $this->handle($exception);
+    }
+
+    private function handle(Exception $exception)
+    {
+        $exception = $this->prepareException($exception);
+
+        return $this->genericResponse($exception);
+    }
+
+    protected function prepareException(Exception $exception)
+    {
+        if ($exception instanceof ModelNotFoundException || $exception instanceof SuspiciousOperationException) {
+            $exception = new NotFoundHttpException(__('http_errors.404'), $exception);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            $exception = new AccessDeniedHttpException(__('http_errors.403'), $exception);
+        }
+
+        if ($exception instanceof TokenMismatchException) {
+            $exception = new HttpException(419, __('http_errors.419'), $exception);
+        }
+
+        return $exception;
+    }
+
+    private function genericResponse(Exception $exception)
+    {
+        return $this->responseError('',
+            $this->convertExceptionToArray($exception),
+            $this->getHeaders($exception),
+            $this->getStatusCode($exception));
+    }
+
+    protected function convertExceptionToArray(Exception $exception) : array
+    {
+        $responses['code'] = $exception instanceof ApiErrorException ? $exception->getApiCode() : $exception->getCode();
+
+        $statusCode = $this->getStatusCode($exception);
+
+        $responses['message'] = $exception->getMessage() ?: sprintf("%d - %s", $statusCode, __("http_errors.$statusCode"));
+
+        if ($exception instanceof ValidationException) {
+            $responses['errors'] = $exception->errors();
+        }
+
+        if ($this->isRunningInDebugMode()) {
+            $responses['debug'] = $this->buildDebugLog($exception);
+        }
+
+        return $responses;
+    }
+
+    private function isRunningInDebugMode() : bool
+    {
+        return config('app.debug');
+    }
+
+    private function buildDebugLog(Exception $exception)
+    {
+        $debugLogs = [
+            'message' => $exception->getMessage(),
+            'exception' => get_class($exception),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+            'trace' => explode("\n", $exception->getTraceAsString()),
+        ];
+
+        if ($exception->getPrevious()) {
+            $debugLogs['trace'] = [
+                'current' => explode("\n", $exception->getTraceAsString()),
+                'previous' => explode("\n", $exception->getPrevious()->getTraceAsString())
+            ];
+        }
+
+        return $debugLogs;
+    }
+
+    private function getHeaders(Exception $exception) : array
+    {
+        return $exception instanceof HttpExceptionInterface ? $exception->getHeaders() : [];
+    }
+
+    private function getStatusCode(Exception $exception) : int
+    {
+        if ($exception instanceof ValidationException) {
+            return $exception->status;
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return Response::HTTP_UNAUTHORIZED;
+        }
+
+        return $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : Response::HTTP_NOT_FOUND;
     }
 }