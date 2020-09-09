03082109753717de9faf491e4c9a6598003db646
sondngithub <duongson29111997@gmail.com>
2020-09-09 14:16:13 +0700

    Add layout template.
    Signed-off-by: sondngithub <duongson29111997@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,12 @@
+import './bootstrap'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import RouterPrefetch from 'vue-router-prefetch'
+import router from "./router/index";
+import BlackDashboard from "./plugins/blackDashboard";
+import i18n from "./i18n";
+import App from "./App";
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -13,20 +22,16 @@ window.Vue = require('vue');
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * Eg. ./components/login.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.use(BlackDashboard);
+Vue.use(VueRouter);
+Vue.use(RouterPrefetch);
 
 const app = new Vue({
     el: '#app',
+    router: router,
+    i18n,
+    render: h => h(App)
 });
